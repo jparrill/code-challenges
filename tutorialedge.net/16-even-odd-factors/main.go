c44cdb0a87e03c4cff6a8e26577f0c373d034aae
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2022-08-17 08:16:25 +0200

    Added challenge even-odd-factors
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

---
+++ tutorialedge.net/16-even-odd-factors/main.go
@@ -0,0 +1,47 @@
+/*
+👋 Welcome Gophers! In this challenge, you will be tasked with implementing a function that will return either “odd” or “even” depending on whether or not a number has an odd or an even number of factors.
+
+Aclaration:
+- Find the factors of an int
+- Check the lenght of the total Factors Found
+- Check if that lenght is even or odd
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func OddEvenFactors(num int) string {
+	factors := CheckFactors(num)
+	fmt.Println("Factors:", factors)
+	if len(factors)%2 == 0 {
+		return "even"
+	}
+	return "odd"
+}
+
+func CheckFactors(num int) []int {
+	factors := []int{1, num}
+	limit := int(math.Ceil(float64(num) / 2))
+	for i := 2; i <= limit; i++ {
+		if num%i == 0 {
+			fmt.Printf("Found: %d\n", i)
+			factors = append(factors, i)
+		}
+	}
+	return factors
+}
+
+func main() {
+	fmt.Println("Odd or Even Factors")
+
+	numFactors := OddEvenFactors(23)
+	fmt.Println(numFactors)     // "even"
+	fmt.Println("------------") // "even"
+
+	numFactors = OddEvenFactors(36)
+	fmt.Println(numFactors) // "odd"
+}
